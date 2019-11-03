3ff29de96c08e35c0cf0bd4d3a272534a5b54500
Rashad Amirjanov <rashad.amirjanov@pashabank.az>
2019-11-03 22:04:25 +0400

    Generalize authentication to github

---
+++ authn.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+
+	"github.com/google/go-github/v28/github"
+	"golang.org/x/oauth2"
+)
+
+func authn(ctx context.Context) *github.Client {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: args.Token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	return client
+}
--- team.go
+++ team.go
@@ -8,14 +8,11 @@ import (
 	"sort"
 
 	"github.com/google/go-github/v28/github"
-	"golang.org/x/oauth2"
 )
 
 func getTeams(org string, format string) {
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: githubToken()})
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
+	client := authn(ctx)
 
 	opt := &github.ListOptions{PerPage: 100}
 	var objsAll []*github.Team
